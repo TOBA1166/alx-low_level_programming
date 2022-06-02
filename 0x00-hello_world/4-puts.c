e35b3ba6cc212af64575dbb06e79d68c5692d004
Adeoluwa Tobalase <adetobs05@gmail.com>
2022-06-02 14:48:39 -0700

    Printing with function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
-
+ *  * main - Entry point
+ *   *
+ *    * Description: prints "Programming is like building a multilingual puzzle"
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
