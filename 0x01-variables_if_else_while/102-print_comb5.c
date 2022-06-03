e8dca44b26d309953e2010f31a941cd0a1841b39
Adeoluwa Tobalase <adetobs05@gmail.com>
2022-06-03 11:29:55 -0700

    README.md

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, p, q, a, b;
+
+	a = 0;
+	b = 0;
+	for (i = 0; i <= 9; ++i)
+	{
+		for (j = 0; j <= 9; ++j)
+		{
+			a = (i * 10) + j;
+			for (p = 0; p <= 9; ++p)
+			{
+				for (q = 0; q <= 9; ++q)
+				{
+					b = (p * 10) + q;
+					if (b > a)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(p + '0');
+						putchar(q + '0');
+					}
+					if ((a == 98) && (b == 99))
+						putchar('\n');
+					else if (b > a)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
+
