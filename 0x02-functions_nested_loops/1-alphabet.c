7536b894d504be1a23a8101a3e75e22f6a2a0cda
Adeoluwa Tobalase <adetobs05@gmail.com>
2022-06-07 01:14:21 -0700

    README.md

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - void to print alphabet
+ *
+ * Return: Nothing.
+ */
+ void print_alphabet(void)
+{
+		char c;
+
+
+		for (c = 'a'; c <= 'z'; c++)
+
+		{
+
+			_putchar(c);
+
+		}							}
+
+		_putchar('\n');
+
+}
