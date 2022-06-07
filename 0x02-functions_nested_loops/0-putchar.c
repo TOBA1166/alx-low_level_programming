c9470f56d9fa25497fa30d5c86bf3bc8a476a383
Adeoluwa Tobalase <adetobs05@gmail.com>
2022-06-07 08:02:17 -0700

    README.md

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-	char hb[] = "_putchar";
+	char hb[] = "_putchar"; h = '/n' ;
 	int i;
 
 	for (i = 0; i <= 9 ; i++)
 	{
 		_putchar(hb[i]);
 	}
+	_putchar(h);
 	return (0);
 }
