d1a3c4e6a66c580969c519b83476de4d40d01dcb
Adeoluwa Tobalase <adetobs05@gmail.com>
2022-06-09 07:01:46 -0700

    README.md

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prin series from 1..100 with Fizz Buzz for i % 3 and i % 5.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, f, b;
+
+	for (i = 1; i <= 98; i++)
+	{
+		f = i % 3;
+		b = i % 5;
+		(f == 0 && b == 0) ? printf("FizzBuzz ") : f == 0 ?
+			printf("Fizz ") : b == 0 ? printf("Buzz ") : printf("%i ", i);
+	}
+	printf("Fizz Buzz\n");
+	return (0);
+}
