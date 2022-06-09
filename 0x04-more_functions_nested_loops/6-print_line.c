67bfb500bf70a54bb8f469c9f5093f6e4b028283
Adeoluwa Tobalase <adetobs05@gmail.com>
2022-06-09 06:50:05 -0700

    README.md

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - Print lines with '_' char.
+ * @n: integer to define n chars to print.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
